05fbc3f25542e318a50a1e1d96e6bf57d3ca67a6
omardcan <38349951+omardcan@users.noreply.github.com>
2018-05-10 12:20:55 -0300

    Fundo se mexe porém se duplica
    Avião se mexe, porém se duplica

--- Jogo.py
+++ Jogo.py
@@ -1,106 +1,105 @@
 import pygame
-import sys
-from pygame.locals import*
+import pygame.locals
 
+
+branco = (255,255,255)
+marrom = (199,111,80)
+cinza = (150,150,150)
+vermelho = (230,0,0)
+preto = (0,0,0)
 
 #======================= CLASSES ====================================
 
 class Aviao(pygame.sprite.Sprite):
     
-    def _init_(self,imagem_aviao,posx,posy):
+    def __init__(self,arquivo_imagem,px,py):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load(arquivo_imagem)
+        self.rect = self.image.get_rect() 
+        self.rect.x = px
+        self.rect.y = py
+        
+    def move(self,velocidade_x,velocidade_y):
+        self.rect.x = velocidade_x
+        self.rect.y = velocidade_y
+
+'''class Tiros(pygame.sprite.Sprite):
+    
+    def _init_(self):
         pygame.sprite.Sprite._init_(self)
-        self.px =  posx
-        self.py = posy
-        self.image = self.image.load(imagem_aviao)
+        self.image = pygame.Surface([4,10])
+        self.image.fill(preto)
+        self.rect = self.image.get_rect()
         
-    def move(self):
-        self.rect.x += self.px
-        self.rect.y += self.py
-        
-    
-#    def danos():
-    
+    def voa(self):
+        self.rect.x += 5
+ '''       
 #====================== INICIAÇÃO ===================================
 
 pygame.init()
 
-tela = pygame.display.set_mode((800,600),0,32)
+tela = pygame.display.set_mode((800, 600),0,32)
 
 pygame.display.set_caption("Jogo A+")
 
-#fundo = pygame.imagem.load(             ).convert()
+fundo = pygame.image.load("imagem_fundo.jpg").convert()
 
 relogio = pygame.time.Clock()
 
-branco = (255,255,255)
-cordebosta = (199,111,80)
-cinza = (108,108,108)
-vermelho =(230,0,0)
+aviao = Aviao("imagem_aviao.png",40,40)
 
+aviao_group = pygame.sprite.Group()
+aviao_group.add(aviao)
+#aviao_group.add(Tiros)
 
-cacto=pygame.rect(10,10,10,10)
-boladefogo=pygame.rect(10,20,10,20)
-
-
-
-
-
-
-
-
+#tela.blit(fundo,(0,0))
 #======================= LOOP PRINCIPAL =============================
 
 def loop():
-
+    y=0
     rodando = True
-    while rodando:   
+    while rodando:
+        
         for event in pygame.event.get():
+            
             
             if event.type == pygame.QUIT:
                 rodando = False
-
+        
+     
             if event.type == pygame.KEYDOWN:
                 
                 if event.key == pygame.K_s or event.key == pygame.K_DOWN: 
-                    aviao.move_ip(0,20)
+                    aviao.move(0,20)
                 
                 if event.key == pygame.K_w or event.key == pygame.K_UP:
-                    aviao.move_ip(0,-20)
-                    
+                    aviao.move(0,-20)
         
                 if event.key == pygame.K_d or event.key == pygame.K_RIGHT:
-                    aviao.move_ip(20,0)
+                    aviao.move(20,0)
         
                 if event.key == pygame.K_a or event.key == pygame.K_LEFT:
-                    aviao.move_ip(-20,0)
+                    aviao.move(-20,0)
         
         
                 if event.key == pygame.K_SPACE:
                     print('tiros')
                     #fazer tiros
-
-
-
-        correndo = True
-        while correndo:
-            pygame.draw.rect(tela,cor,cacto)
-            cacto.move_ip(20,0)
-            pygame.draw.rect(tela,vermelho,boladefogo)
-            cacto.move_ip(15,0)
-            
+                
         
-        
-        
-        tela.fill(branco)
-        relogio.tick(30)
-        
-        
-        # aviao e balao sao representados por retangulos ainda
-        pygame.draw.rect(tela,cordebosta,aviao)
-        pygame.draw.rect(tela,cinza,balao)
-        
-        
+        relogio.tick(27)
+        aviao_group.draw(fundo)
+    
+        y-=1
+        rel_x = y % fundo.get_rect().width
+        tela.blit(fundo,(rel_x-fundo.get_rect().width,0))
+        if rel_x<800:
+            tela.blit(fundo,(rel_x,0))
+        #tela.blit(aviao_group,Aviao.move)
         pygame.display.update()    
+        pygame.display.flip()
+        
+        
     pygame.quit()
 
 loop()
