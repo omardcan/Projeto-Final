191a5f77ab215d7c358b08f6f1753710e574a8f1
gDuarteg <38353564+gDuarteg@users.noreply.github.com>
2018-05-18 08:49:05 -0300

    aviao se move pelas teclas

--- Jogo.py
+++ Jogo.py
@@ -1,23 +1,18 @@
 import pygame
-import sys
-from pygame.locals import*
+import pygame.locals
 
 #======================= CLASSES ====================================
 
-class Aviao(pygame.sprite.Sprite):
-    
-    def _init_(self,imagem_aviao,posx,posy):
-        pygame.sprite.Sprite._init_(self)
-        self.px =  posx
-        self.py = posy
-        self.image = self.image.load(imagem_aviao)
-        
-    def move(self):
-        self.rect.x += self.px
-        self.rect.y += self.py
-        
-    
-#    def danos():
+class Aviao(pygmae.sprite.Sprite):
+    def __init__(self,arquivo_imagem,px,py):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load(arquivo_imagem)
+        self.rect = self.image.get_rect() 
+        self.rect.x = px
+        self.rect.y = py
+    def move(self,velocidade_x,velocidade_y):
+        self.rect.x += velocidade_x
+        self.rect.y += velocidade_y
     
 #====================== INICIAÇÃO ===================================
 pygame.init()
@@ -26,11 +21,19 @@ tela = pygame.display.set_mode((800,600),0,32)
 
 pygame.display.set_caption("Jogo A+")
 
-#fundo = pygame.imagem.load(             ).convert()
+fundo = pygame.image.load("imagem_fundo.jpg").convert()
 
 relogio = pygame.time.Clock()
 
 branco = (255,255,255)
+
+marrom = (199,111,80)
+cinza = (150,150,150)
+vermelho = (230,0,0)
+
+aviao = Aviao("imagem_nave.jpeg",40,40)
+aviao_group = pygame.sprite.Group()
+aviao_group.add(aviao)
 
 #======================= LOOP PRINCIPAL =============================
 def loop():
@@ -41,10 +44,31 @@ def loop():
             
             if event.type == pygame.QUIT:
                 rodando = False
+            if event.type == pygame.KEYDOWN:
+                
+                if event.key == pygame.K_s or event.key == pygame.K_DOWN: 
+                    aviao.move(0,20)
+                
+                if event.key == pygame.K_w or event.key == pygame.K_UP:
+                    aviao.move(0,-20)
         
-        tela.fill(branco)
-        relogio.tick(30)
+                if event.key == pygame.K_d or event.key == pygame.K_RIGHT:
+                    aviao.move(20,0)
+        
+                if event.key == pygame.K_a or event.key == pygame.K_LEFT:
+                    aviao.move(-20,0)
+        
+        
+                if event.key == pygame.K_SPACE:
+                    print('tiros')
+                    #fazer tiros
+                    
+                        
+        relogio.tick(27)
+        
+        tela.blit(fundo,(0,0))
+        aviao_group.draw(fundo)
         pygame.display.update()    
+        pygame.display.flip()
     pygame.quit()
-
 loop()
