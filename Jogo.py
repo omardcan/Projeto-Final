2fb52b1899d4db1b97f7403cc740ed257541ff99
gDuarteg <38353564+gDuarteg@users.noreply.github.com>
2018-05-18 09:18:26 -0300

    aviao se move continuamente

--- Jogo.py
+++ Jogo.py
@@ -16,16 +16,56 @@ class Aviao(pygmae.sprite.Sprite):
         self.rect = self.image.get_rect() 
         self.rect.x = px
         self.rect.y = py
-    def move(self,velocidade_x,velocidade_y):
-        self.rect.x += velocidade_x
-        self.rect.y += velocidade_y
+     def move(self):
+        self.velx = 0
+        self.vely = 0
+        key = pygame.key.get_pressed()
+        if self.rect.x >= 0:
+            if key[pygame.K_LEFT] or key[pygame.K_a]:
+                self.velx = -8
+        if self.rect.x <= 600:
+            if key[pygame.K_RIGHT] or key[pygame.K_d]:
+                self.velx = 8
+        if self.rect.y > 0:
+            if key[pygame.K_UP] or key[pygame.K_w]:
+                self.vely = -8 
+        if self.rect.y <= 500:
+            if key[pygame.K_DOWN] or key[pygame.K_s]:
+                self.vely = 8
+        self.rect.x += self.velx
+        self.rect.y += self.vely
 
 class Balao(pygame.sprite.Sprite):
-    def __init__(self,arquivo_imagem):
+    def __init__(self,arquivo_imagem,px,py):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.image.load(arquivo_imagem)
-        self.rect = self.image.get_rect()        
-  
+        self.rect = self.image.get_rect()
+        self.rect.x = px
+        self.rect.y = py
+    def move(self):
+        self.velx = 0
+        self.vely = 0
+
+class Fundilho(pygame.sprite.Sprite):
+    def __init__(self,arquivo_imagem,px,py):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load(arquivo_imagem)
+        self.rect = self.image.get_rect()
+        self.image = pygame.transform.scale(self.image,(200,100))
+        self.rect.x = px
+        self.rect.y = py         
+    def move(self):
+        self.rect.x
+        self.rect.y
+class Tiros(pygame.sprite.Sprite):
+    
+    def __init__(self,arquivo_imagem,px,py):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load(arquivo_imagem)
+        self.rect = self.image.get_rect()
+        self.image = pygame.transform.scale(self.image,(200,100))
+        self.rect.x = px
+        self.rect.y = py
 #====================== INICIAÇÃO ===================================
 pygame.init()
 
@@ -45,8 +85,7 @@ aviao_group.add(aviao)
 
 for i in range(2):
     balao = Balao("imagem_balao.png")
-    balao.rect.x = random.randrange(800)
-    balao.rect.y = random.randrange(600)
+    balao = Balao("imagem_balao.png",random.randrange(750,800),random.randrange(600))
     balao_group.add(balao)
 #======================= LOOP PRINCIPAL =============================
 def loop():
@@ -59,24 +98,17 @@ def loop():
                 rodando = False
             if event.type == pygame.KEYDOWN:
                 
-                if event.key == pygame.K_s or event.key == pygame.K_DOWN: 
-                    aviao.move(0,20)
-                
-                if event.key == pygame.K_w or event.key == pygame.K_UP:
-                    aviao.move(0,-20)
-        
-                if event.key == pygame.K_d or event.key == pygame.K_RIGHT:
-                    aviao.move(20,0)
-        
-                if event.key == pygame.K_a or event.key == pygame.K_LEFT:
-                    aviao.move(-20,0)
-        
-        
                 if event.key == pygame.K_SPACE:
                     print('tiros')
                     #fazer tiros
-                    
-                        
+        aviao.move()            
+        y-=1
+        rel_x = y % fundo.get_rect().width
+        tela.blit(fundo,(rel_x-fundo.get_rect().width,0))
+        if rel_x<1920:
+        tela.blit(fundo,(rel_x,0))
+        aviao_group.draw(tela)         
+        balao_group.draw(tela)                     
         relogio.tick(27)
         
         tela.blit(fundo,(0,0))
